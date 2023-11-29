a99dcce9645e7f1bd524b259dbb7a4901438efc1
Buttersnow <b4.fly4@gmail.com>
2023-11-28 22:21:19 -0600

    assignment 4.3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-//This gives me the my name and current year
+//This gives me the my name and curreAssignment 4.2nt year at the botton of the page 4.2
     var today = new Date();
     var thisYear = today.getFullYear();
     var footer = document.querySelector('footer');
     var copyright = document.createElement('p');
     copyright.innerHTML = `Tilla ${thisYear}`;
     footer.appendChild(copyright);
+
 // Adding list of my skillset.
 var Skills = ["HTML", "Badminton", "Cricket"];
 var skillsSection = document.getElementById('Skills');
@@ -14,5 +15,47 @@ for (var i = 0; i < Skills.length; i++) {
     skill.innerText = Skills[i];
     skillsList.appendChild(skill);
 }
+// Selecting the form by name attribute to leave message, show on the page and delete button. Assignment 4.3
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const usersName = event.target.usersName.value;
+  const usersEmail = event.target.usersEmail.value;
+  const usersMessage = event.target.usersMessage.value;
+
+  console.log(usersName);
+  console.log(usersMessage);
+  console.log(usersEmail);
+
+
+  event.target.reset();
+
+  const messageSection = document.querySelector('#messages');
+  const messageList = messageSection.querySelector('ul');
+
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>
+  `;
+
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'remove';
+  removeButton.type = 'button';
+
+  removeButton.addEventListener('click', function() {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+
+  messageList.appendChild(newMessage);
+});
+
+
+
 
 
